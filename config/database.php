33c635310edaf578a1211e1922eaf8f6251e5ec5
hiimtaylorjones <taylorjonessoftware@gmail.com>
2017-08-06 20:13:13 -0400

    database config

--- config/database.php
+++ config/database.php
@@ -1,31 +1,18 @@
 <?php
 
-$url = parse_url(getenv("DATABASE_URL"));
-$host = $url["host"];
-$username = $url["user"];
-$password = isset($url["pass"]) ? $url["pass"] : '';
-$database = substr($url["path"], 1);
-
 return [
-
     'default' => env('DB_CONNECTION', 'pgsql'),
-
     'connections' => [
-
-        'pgsql' => [
-            'driver' => 'pgsql',
-            'host' => $host,
-            'database' => $database,
-            'username' => $username,
-            'password' => $password,
-            'charset' => 'utf8',
-            'prefix' => '',
-            'schema' => 'public',
-            'sslmode' => 'prefer',
-        ],
-
+      'pgsql' => [
+          'driver' => 'pgsql',
+          'host' => env('DB_HOST', 'localhost'),
+          'database' => env('DB_DATABASE', 'forge'),
+          'username' => env('DB_USERNAME', 'forge'),
+          'password' => env('DB_PASSWORD', ''),
+          'charset' => 'utf8',
+          'prefix' => '',
+          'schema' => 'public',
+      ],
     ],
-
     'migrations' => 'migrations'
-
 ];
